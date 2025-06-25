447964eeebed9a271f88c4e27bf5052ec911d4a7
yaonur <ya_onur@hotmail.com>
2025-06-25 03:13:47 +0300

    Update PFretboard exercises with corrected bass string configurations
    Modified the bass string values for several exercises in the PFretboard component to ensure accurate playback. The changes include updating the bass strings for "maj6/7 skipping," "root 7 chords (treble)," and "root 7 chords (bass)" to enhance the overall practice experience.

--- front/src/lib/components/pfretboard/practices.ts
+++ front/src/lib/components/pfretboard/practices.ts
@@ -1,17 +1,18 @@
+
 export const exercises = {
 	"maj6/7 skipping": {
 		chordName: "6/7",
-		bassString: 1,
+		bassString: 6,
 		structure:  [[1], [5], [6], [3], [5], [7, 1]]
 	},
 	"root 7 chords (treble)":{
 		chordName: "7",
-		bassString: 3,
+		bassString: 4,
 		structure: [[],[],[1],[5],[7],[3]]
 	},
 	"root 7 chords (bass)": {
 		chordName: "7",
-		bassString: 2,
+		bassString: 5,
 		structure: [[],[1],[5],[7],[3],[]]
 	}
 	 
