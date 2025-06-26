698708266b6afaf270e5d4cfa100d1ba7b6011ba
yaonur <ya_onur@hotmail.com>
2025-06-26 22:20:14 +0300

    Add new chord and triad exercises to Fretboard component
    Expanded the exercises in the Fretboard component by adding various shell chords and triads in different root positions. This enhancement provides users with a broader range of practice options, improving their learning experience and versatility in playing. Each exercise includes detailed structure and string information for better guidance during practice sessions.

--- front/src/lib/components/pfretboard/practices.ts
+++ front/src/lib/components/pfretboard/practices.ts
@@ -97,6 +97,125 @@ export const exercises = {
 	"rooot 7 arpegio (treble)": {
 		bassString: 5,
 		structure: [[],[1,3],[5], [7,1],[3],[5,7,5]]
-	}
-	 
+	},
+	"shell 7 chord (root 6, maj7)": {
+		chordName: "maj7 shell",
+		bassString: 6,
+		structure: [[1],[],[7],[3],[],[]],
+		rootString: 6,
+		thirdString: 4,
+		seventhString: 3
+	},
+	"shell 7 chord (root 5, maj7)": {
+		chordName: "maj7 shell",
+		bassString: 5,
+		structure: [[],[1],[],[7],[3],[]],
+		rootString: 5,
+		thirdString: 2,
+		seventhString: 3
+	},
+	"shell 7 chord (root 4, maj7)": {
+		chordName: "maj7 shell",
+		bassString: 4,
+		structure: [[],[],[1],[],[7],[3]],
+		rootString: 4,
+		thirdString: 1,
+		seventhString: 2
+	},
+	"triad (root 6, maj)": {
+		chordName: "maj triad",
+		bassString: 6,
+		structure: [[1],[3],[5],[],[],[]],
+		rootString: 6,
+		thirdString: 5,
+		fifthString: 4
+	},
+	"triad (root 5, maj)": {
+		chordName: "maj triad",
+		bassString: 5,
+		structure: [[],[1],[3],[5],[],[]],
+		rootString: 5,
+		thirdString: 4,
+		fifthString: 3
+	},
+	"triad (root 4, maj)": {
+		chordName: "maj triad",
+		bassString: 4,
+		structure: [[],[],[1],[3],[5],[]],
+		rootString: 4,
+		thirdString: 3,
+		fifthString: 2
+	},
+	"triad (root 3, maj)": {
+		chordName: "maj triad",
+		bassString: 3,
+		structure: [[],[],[],[1],[3],[5]],
+		rootString: 3,
+		thirdString: 2,
+		fifthString: 1
+	},
+	"triad (root 6, maj) 1.inv": {
+		chordName: "maj triad 1.inv",
+		bassString: 6,
+		structure: [[3],[5],[1],[],[],[]],
+		rootString: 4,
+		thirdString: 6,
+		fifthString: 5
+	},
+	"triad (root 5, maj) 1.inv": {
+		chordName: "maj triad 1.inv",
+		bassString: 5,
+		structure: [[],[3],[5],[1],[],[]],
+		rootString: 3,
+		thirdString: 5,
+		fifthString: 4
+	},
+	"triad (root 4, maj) 1.inv": {
+		chordName: "maj triad 1.inv",
+		bassString: 4,
+		structure: [[],[],[3],[5],[1],[]],
+		rootString: 2,
+		thirdString: 4,
+		fifthString: 3
+	},
+	"triad (root 3, maj) 1.inv": {
+		chordName: "maj triad 1.inv",
+		bassString: 3,
+		structure: [[],[],[],[3],[5],[1]],
+		rootString: 1,
+		thirdString: 3,
+		fifthString: 2
+	},
+	"triad (root 6, maj) 2.inv": {
+		chordName: "maj triad 2.inv",
+		bassString: 6,
+		structure: [[5],[1],[3],[],[],[]],
+		rootString: 5,
+		thirdString: 4,
+		fifthString: 6
+	},
+	"triad (root 5, maj) 2.inv": {
+		chordName: "maj triad 2.inv",
+		bassString: 5,
+		structure: [[],[5],[1],[3],[],[]],
+		rootString: 4,
+		thirdString: 3,
+		fifthString: 5
+	},
+	"triad (root 4, maj) 2.inv": {
+		chordName: "maj triad 2.inv",
+		bassString: 4,
+		structure: [[],[],[5],[1],[3],[]],
+		rootString: 3,
+		thirdString: 2,
+		fifthString: 4
+	},
+	"triad (root 3, maj) 2.inv": {
+		chordName: "maj triad 2.inv",
+		bassString: 3,
+		structure: [[],[],[],[5],[1],[3]],
+		rootString: 2,
+		thirdString: 1,
+		fifthString: 3
+	},
 };
